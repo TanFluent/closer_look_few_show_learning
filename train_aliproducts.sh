a2089200e5b8f0cc1469d8147db4c501c994a619
高手 <fulun.tfl@alibaba-inc.com>
2020-05-05 21:18:34 +0800

    update: training aliproducts

--- train_aliproducts.sh
+++ train_aliproducts.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
+# logging
+# https://stackoverflow.com/questions/12919980/nohup-is-not-writing-log-to-output-file
+
 # baseline/baseline++
+nohup python -u ./train.py --dataset ali_products --model ResNet50 --method baseline++ --train_aug > out.log &
 
 # meta-learning
-#python ./train.py --dataset ali_products --model Conv4 --method protonet --train_n_way 5 --n_shot 5 --test_n_way 5 --train_aug
-python ./train.py --dataset ali_products --model ResNet50 --method baseline++ --train_aug
+python ./train.py --dataset ali_products --model Conv4 --method protonet --train_n_way 5 --n_shot 5 --test_n_way 5 --train_aug
