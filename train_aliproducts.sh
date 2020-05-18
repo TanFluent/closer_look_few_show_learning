f033c89774ebeb804fb86448f919a4faa35e667e
高手 <fulun.tfl@alibaba-inc.com>
2020-05-18 20:29:20 +0800

    update: training aliproducts

--- train_aliproducts.sh
+++ train_aliproducts.sh
@@ -8,3 +8,9 @@ nohup python -u ./train.py --dataset ali_products --model ResNet50 --method base
 
 # meta-learning
 python ./train.py --dataset ali_products --model Conv4 --method protonet --train_n_way 5 --n_shot 5 --test_n_way 5 --train_aug
+
+# save-feature
+python save_features.py --dataset ali_products --model ResNet50 --method baseline++ --train_aug
+
+# test
+python test.py --dataset ali_products --model ResNet50 --method baseline++ --train_aug
